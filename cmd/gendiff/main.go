6c12345a2613a65c536b3ce4a7b3d124127efb9a
Denis Zhdanov <altwazar@gmail.com>
2025-08-18 15:58:01 +0700

    add format flag

--- cmd/gendiff/main.go
+++ cmd/gendiff/main.go
@@ -9,10 +9,19 @@ import (
 
 func main() {
 	//	(&cli.Command{}).Run(context.Background(), os.Args)
+	var format string
 	cmd := &cli.Command{
 		Name:      "gendiff",
 		Usage:     "Compares two configuration files and shows a difference.",
 		UsageText: "gendiff [global options]",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "format",
+				Aliases:     []string{"f"},
+				Value:       "stylish",
+				Destination: &format,
+			},
+		},
 	}
 
 	if err := cmd.Run(context.Background(), os.Args); err != nil {
