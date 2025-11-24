7627d38c19c34b9f1a20b94d97094cf7eced5c6f
Denis Zhdanov <altwazar@gmail.com>
2025-11-24 16:45:19 +0700

    split plain output code to functions and add builder

--- formatters/plain.go
+++ formatters/plain.go
@@ -9,64 +9,98 @@ import (
 
 // Рекурсивный разбор изменений в плоском формате
 func formatOutputPlain(rootDiff parsers.Diff) string {
-	var out string
-	out += formatDiffChildPlain(rootDiff.DiffChild, "")
-	out = strings.TrimSuffix(out, "\n")
-	return out
+	out := formatDiffChildPlain(rootDiff.DiffChild, "")
+	return strings.TrimSuffix(out, "\n")
 }
+
 func formatDiffChildPlain(diffs []parsers.Diff, path string) string {
+	builder := &plainBuilder{
+		path:  path,
+		diffs: diffs,
+	}
+	return builder.build()
+}
+
+type plainBuilder struct {
+	path  string
+	diffs []parsers.Diff
+}
+
+func (b *plainBuilder) build() string {
+	b.sortDiffs()
+
 	var out string
-	// Сортировка по ключам
-	sort.Slice(diffs, func(i, j int) bool {
-		return diffs[i].Key < diffs[j].Key
-	})
-	var newKey string
-	var oldKey string
-	var pathKey string
-	for _, diff := range diffs {
-		// Значения ключей в нужном формате
-		if diff.New == nil {
-			newKey = "null"
-		} else if isString(diff.New) {
-			newKey = fmt.Sprintf("'%v'", diff.New)
-		} else {
-			newKey = fmt.Sprintf("%v", diff.New)
-		}
-		if diff.Old == nil {
-			oldKey = "null"
-		} else if isString(diff.Old) {
-			oldKey = fmt.Sprintf("'%v'", diff.Old)
-		} else {
-			oldKey = fmt.Sprintf("%v", diff.Old)
-		}
-		// Путь ключа для plain вывода
-		if path != "" {
-			pathKey = path + "." + diff.Key
-		} else {
-			pathKey = diff.Key
-		}
-		// Формирование тела вывода, для вложенных структур рекурсивно вызывается эта же функция
-		switch diff.State {
-		case parsers.Updated:
-			switch {
-			case !diff.OldIsMap && !diff.NewIsMap:
-				out += fmt.Sprintf("Property '%s' was updated. From %s to %s\n", pathKey, oldKey, newKey)
-			case diff.OldIsMap && !diff.NewIsMap:
-				out += fmt.Sprintf("Property '%s' was updated. From [complex value] to %s\n", pathKey, newKey)
-			case !diff.OldIsMap && diff.NewIsMap:
-				out += fmt.Sprintf("Property '%s' was updated. From %s to [complex value]\n", pathKey, oldKey)
-			default:
-				out += formatDiffChildPlain(diff.DiffChild, pathKey)
-			}
-		case parsers.Added:
-			if !diff.OldIsMap && !diff.NewIsMap {
-				out += fmt.Sprintf("Property '%s' was added with value: %s\n", pathKey, newKey)
-			} else {
-				out += fmt.Sprintf("Property '%s' was added with value: [complex value]\n", pathKey)
-			}
-		case parsers.Removed:
-			out += fmt.Sprintf("Property '%s' was removed\n", pathKey)
-		}
+	for _, diff := range b.diffs {
+		out += b.buildDiffLine(diff)
 	}
 	return out
+}
+
+func (b *plainBuilder) sortDiffs() {
+	sort.Slice(b.diffs, func(i, j int) bool {
+		return b.diffs[i].Key < b.diffs[j].Key
+	})
+}
+
+func (b *plainBuilder) buildDiffLine(diff parsers.Diff) string {
+	switch diff.State {
+	case parsers.Updated:
+		return b.buildUpdatedLine(diff)
+	case parsers.Added:
+		return b.buildAddedLine(diff)
+	case parsers.Removed:
+		return b.buildRemovedLine(diff)
+	default:
+		return ""
+	}
+}
+
+func (b *plainBuilder) buildUpdatedLine(diff parsers.Diff) string {
+	pathKey := b.buildPathKey(diff.Key)
+
+	switch {
+	case !diff.OldIsMap && !diff.NewIsMap:
+		return fmt.Sprintf("Property '%s' was updated. From %s to %s\n",
+			pathKey, b.formatValue(diff.Old), b.formatValue(diff.New))
+	case diff.OldIsMap && !diff.NewIsMap:
+		return fmt.Sprintf("Property '%s' was updated. From [complex value] to %s\n",
+			pathKey, b.formatValue(diff.New))
+	case !diff.OldIsMap && diff.NewIsMap:
+		return fmt.Sprintf("Property '%s' was updated. From %s to [complex value]\n",
+			pathKey, b.formatValue(diff.Old))
+	default:
+		return formatDiffChildPlain(diff.DiffChild, pathKey)
+	}
+}
+
+func (b *plainBuilder) buildAddedLine(diff parsers.Diff) string {
+	pathKey := b.buildPathKey(diff.Key)
+
+	if !diff.OldIsMap && !diff.NewIsMap {
+		return fmt.Sprintf("Property '%s' was added with value: %s\n",
+			pathKey, b.formatValue(diff.New))
+	}
+	return fmt.Sprintf("Property '%s' was added with value: [complex value]\n", pathKey)
+}
+
+func (b *plainBuilder) buildRemovedLine(diff parsers.Diff) string {
+	pathKey := b.buildPathKey(diff.Key)
+	return fmt.Sprintf("Property '%s' was removed\n", pathKey)
+}
+
+func (b *plainBuilder) buildPathKey(key string) string {
+	if b.path != "" {
+		return b.path + "." + key
+	}
+	return key
+}
+
+func (b *plainBuilder) formatValue(value interface{}) string {
+	if value == nil {
+		return "null"
+	}
+	if isString(value) {
+		return fmt.Sprintf("'%v'", value)
+	}
+	return fmt.Sprintf("%v", value)
 }
